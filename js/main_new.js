86e705ff1850b59eba07a392d36e14ef08caa57a
hbizsprt <119561576+hbizsprt@users.noreply.github.com>
2023-04-23 16:18:36 +0900

    Add files via upload

--- js/main_new.js
+++ js/main_new.js
@@ -16,17 +16,28 @@
     if(openningCurrentIndex===heroMessages.length){
       heroMask.classList.remove('appear');
         clearTimeout(timeoutId);
+        heroImagesSlide();
       }
       
 
     console.log(openningCurrentIndex);
 
   }
-  function heroMessageSlide(){
+  const heroImages=document.querySelectorAll('#heroImageArea img');
+  let currentIndex=0;
+  function heroImagesSlide(){
+    setTimeout(heroImagesSlide,5000);
+    heroImages.forEach((image)=>{
+      image.classList.remove('appear');
+    });
+    heroImages[currentIndex].classList.add('appear');
+    currentIndex++;
+    if(currentIndex>heroImages.length-1){
+      currentIndex=0
+    }
 
   }
   window.setTimeout(openning,3000)
-
 
   // ------------SPMENU OPEN------------
   const spMenu=document.getElementById('sp_menu');
